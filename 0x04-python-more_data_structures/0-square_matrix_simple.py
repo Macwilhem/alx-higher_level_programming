5a763bd9442f24ade752c73807b5bca38683ca16
Macwilhem <macwilhem@gmail.com>
2023-01-11 15:32:46 -0800

    more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-    return [[item**2 for item in row] for row in matrix]
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
