5f1b5a2d84d4630463a38c26dbf9b3aea5bea1ea
Macwilhem <macwilhem@gmail.com>
2023-02-09 10:58:30 -0800

    input and output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module with to_json_string method"""
+import json
+
+def to_json_string(my_obj):
+    """Method that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
